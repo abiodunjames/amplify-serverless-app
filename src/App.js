89e702e6315cb04dc09c6df7fbb33aeaf925da79
Dillion Megida <dillionmegida@gmail.com>
2020-08-17 07:16:51 +0100

    update app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @format */
+
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import { withAuthenticator } from '@aws-amplify/ui-react'
+
+import { createTodo } from "./graphql/mutations";
+import { listTodos } from "./graphql/queries";
+
+import Amplify, { API, graphqlOperation } from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [allTodos, setAlltodos] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const todos = await API.graphql(graphqlOperation(listTodos));
+			setAlltodos(todos.data.listTodos.items);
+		})();
+	}, []);
+
+	const [name, setName] = useState("");
+	const [description, setTodoDesc] = useState("");
+
+	const changeTodoName = (e) => {
+		const {
+			target: { value },
+		} = e;
+		setName(value);
+	};
+
+	const changeTodoDesc = (e) => {
+		const {
+			target: { value },
+		} = e;
+		setTodoDesc(value);
+	};
+
+	const submitAddTodo = async (e) => {
+		e.preventDefault();
+		changeTodoName("");
+		changeTodoDesc("");
+		if (name === "" || description === "")
+			return alert("Input fields cannot be empty");
+
+		const todo = { name, description };
+		await API.graphql(graphqlOperation(createTodo, { input: todo }));
+		setAlltodos([todo, ...allTodos]);
+	};
+
+	return (
+		<div className="App">
+			<h1>Todos with GraphQL</h1>
+			<form onSubmit={submitAddTodo}>
+				<input placeholder="Enter todo title " onChange={changeTodoName} />
+				<input
+					placeholder="Enter todo description "
+					onChange={changeTodoDesc}
+				/>
+				<input type="submit" value="Create todo" />
+			</form>
+			{allTodos.map(({ id, name, description }) => (
+				<div key={id}>
+					<h2>{name}</h2>
+					<p>{description}</p>
+				</div>
+			))}
+		</div>
+	);
 }
 
-export default App;
+export default withAuthenticator(App);
